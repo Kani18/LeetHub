a0d7834b6d75a138025e525125d87553a58c59ca
Kanishka D <81683036+Kani18@users.noreply.github.com>
2022-12-01 06:13:43 +0530

    Time: 3 ms (29.56%), Space: 54.4 MB (26.37%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,6 +1,6 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int flag=0;
+    /*    int flag=0;
          for(int row=0;row<matrix.length;row++){
           for(int col=0;col<matrix[row].length;col++){
            
@@ -31,8 +31,36 @@ class Solution {
        }
     }
     
-    
-    
-    
-    
+}*/
+        
+        
+        int rsize=matrix.length;
+        int csize=matrix[0].length;
+        int[][] narr=new int[rsize][csize];
+        
+        
+        //new array and copy it
+        for(int r=0;r<matrix.length;r++){
+            for(int c=0;c<matrix[r].length;c++){
+                narr[r][c]=matrix[r][c];
+            }
+        }
+        
+         for(int r=0;r<matrix.length;r++){
+            for(int c=0;c<matrix[r].length;c++){
+                if(matrix[r][c]==0){
+                    for(int k=0;k<narr[r].length;k++)
+                        narr[r][k]=0;
+                    for(int k=0;k<narr.length;k++)
+                        narr[k][c]=0;
+                }
+            }
+         }
+        
+         for(int r=0;r<narr.length;r++){
+            for(int c=0;c<narr[r].length;c++){
+                matrix[r][c]=narr[r][c];
+            }
+        }
+    }
 }
