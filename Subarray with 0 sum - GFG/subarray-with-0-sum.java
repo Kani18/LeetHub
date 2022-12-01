e94bff55eae8eb269c7b82e69ec6412c4182f267
Kanishka D <81683036+Kani18@users.noreply.github.com>
2022-12-01 22:23:48 +0530

    Added solution - LeetHub

---
+++ Subarray with 0 sum - GFG/subarray-with-0-sum.java
@@ -0,0 +1,86 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		//code
+			//code
+			
+		//taking input using class Scanner
+		Scanner scan = new Scanner(System.in);
+		
+		//taking total number of testcases
+		int t = scan.nextInt();
+		while(t>0)
+		{
+		    //taking total count of elements
+		    int n = scan.nextInt() ;
+		    
+		    //Declaring and Initializing an array of size n
+		    int arr[] = new int[n];
+		    
+		    //adding elements to the array
+		    for(int i = 0; i<n;i++)
+		    {
+		        arr[i] = scan.nextInt();
+		    }
+		    
+		    t--;
+		    
+		     //calling the method findSum
+		     //and print "YES" or "NO" based on the result
+		     System.out.println(new Solution().findsum(arr,n)==true?"Yes":"No");
+		}
+	}
+	
+	
+}
+// } Driver Code Ends
+
+
+class Solution{
+    //Function to check whether there is a subarray present with 0-sum or not.
+    static boolean findsum(int arr[],int n)
+    {
+        //Your code here
+     /*   boolean ans=false;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i]==0)
+               ans=true;
+        }
+        
+        
+        int sum;
+        for(int i=0;i<n;i++){
+            sum=arr[i];
+            for(int j=i+1;j<n;j++)
+             {   sum+=arr[j];
+                 if(sum==0)
+                    ans=true;
+             }
+        }
+        
+        return ans;
+    }
+}*/
+
+
+        //boolean ans=false;
+        HashMap<Integer,Integer> mp=new HashMap<>();
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+          sum+=arr[i];
+          if(mp.containsKey(sum) || sum==0)
+            return true;
+          else
+              mp.put(sum,i);
+         
+        }
+        return false;
+        
+    }
+}
+
