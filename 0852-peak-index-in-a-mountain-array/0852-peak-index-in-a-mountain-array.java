1237b8e6ee266f5e4a213f359439979cfce281b6
Kanishka D <81683036+Kani18@users.noreply.github.com>
2023-01-15 11:16:08 +0530

    Time: 0 ms (100.00%), Space: 59.2 MB (84.24%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,6 +1,6 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-         int max=0;
+        /* int max=0;
         int ans=0;
         for(int i=0;i<arr.length;i++){
             if(arr[i]>max){
@@ -9,5 +9,19 @@ class Solution {
             }
         }
         return ans;
+    }
+}*/
+        
+int l=0;
+int h=arr.length;
+        
+while(l<h){
+    int m=l+(h-l)/2;
+    if(arr[m]>arr[m+1])
+         h=m;
+    else
+        l=m+1;
+}
+        return l;
     }
 }
