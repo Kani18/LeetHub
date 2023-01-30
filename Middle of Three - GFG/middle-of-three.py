ac4a9b6806c0d4903d44ca66d3deb35911cd19aa
Kanishka D <81683036+Kani18@users.noreply.github.com>
2023-01-30 21:07:37 +0530

    Added solution - LeetHub

---
+++ Middle of Three - GFG/middle-of-three.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+
+class Solution:
+    def middle(self,A,B,C):
+        #code here
+        a=[]
+        a.extend([A,B,C])
+        a.sort()
+        
+        return a[1]
+
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__=='__main__':
+    t=int(input())
+    for _ in range(t):
+        A,B,C=map(int,input().strip().split())
+        ob=Solution()
+        print(ob.middle(A,B,C))
+# } Driver Code Ends
