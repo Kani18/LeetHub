40776618f6fbd7779fe0df58a8e88f1004366529
Kanishka D <81683036+Kani18@users.noreply.github.com>
2023-02-01 21:07:25 +0530

    Time: 47 ms (54.32%), Space: 14.4 MB (15.08%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,8 +1,28 @@
+def binarysearch(nums,target,l,h):
+    while(l<=h):
+        mid=l+(h-l)//2
+        if(target > nums[mid]):
+            l=mid+1
+        elif(target < nums[mid]):
+            h=mid-1
+        else:
+            return mid
+    return -1
+
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        n=len(nums)
-        for i in range(n):
-            if(nums[i]==target):
-                return i
-            
-        return -1
+        
+         n=len(nums)
+         if(n==1 and nums[0]!=target):
+                return -1
+         if(n==1 and nums[0]==target):
+               return 0
+         pivot=0
+         for i in range(len(nums)-1):
+            if(nums[i+1]<nums[i]):
+                pivot=i
+                
+         firsthalf=binarysearch(nums,target,0,pivot)
+         if(firsthalf!=-1):
+                return firsthalf
+         return binarysearch(nums,target,pivot+1,n-1)
